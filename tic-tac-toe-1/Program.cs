ea358904f84e36b543e1cbd3344d81c0ded671b1
smoladnd <tomica.smola@gmail.com>
2021-11-10 15:38:51 +0100

    Added three new tic-tac-toe games

---
+++ tic-tac-toe-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tomislav ima 0 pobjeda i 0 poraza!");
+            Console.WriteLine("Mislav ima 0 pobjeda i 0 poraza!");
+
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+        }
+    }
+}
---
+++ tic-tac-toe-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tic_tac_toe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tomislav ima 0 pobjeda i 0 poraza!");
+            Console.WriteLine("Mislav ima 0 pobjeda i 0 poraza!");
+
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   |   ");
+        }
+    }
+}
