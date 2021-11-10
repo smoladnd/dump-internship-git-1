ea09d33cccf6884bc29dfc53b162eb05e2780c9c
smoladnd <tomica.smola@gmail.com>
2021-11-10 17:17:07 +0100

    Conflicts resolved

--- tic-tac-toe-1/Program.cs
+++ tic-tac-toe-1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
             Console.WriteLine("Mislav ima 0 pobjeda i 0 poraza!");
 
             Console.WriteLine("   |   | 0 ");
-            Console.WriteLine("   |   | x ");
+            Console.WriteLine("   | 0 | x ");
             Console.WriteLine("   |   | x ");
         }
     }
